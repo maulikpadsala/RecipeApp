2d00c41fe8ac81ab42408436f92cc4021d56c6cf
Maulik <padsalamaulik1@gmail.com>
2021-08-19 23:10:36 +0530

    Add REcipe UI test cases

---
+++ RecipeAppTest/RecipeUITest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RecipeAppTest
+{
+    public class RecipeUITest: IDisposable
+    {
+        private readonly IWebDriver driver= new ChromeDriver();
+        public RecipeUITest()
+        {
+
+        }
+        public void Dispose()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        [Fact]
+        public void ReturnIndexRecipeView()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44387/Home/Index");
+            Assert.Equals("All-Recipe", driver.Title);            
+        }
+
+        [Fact]
+        public void CrateNewRecipe()
+        {
+            driver.Navigate()
+                .GoToUrl("https://localhost:5001/Employees/Create");
+
+            driver.FindElement(By.Id("RecipeName")).SendKeys("Test RecipeName");
+
+            driver.FindElement(By.Id("Ingredients")).SendKeys("Test Ingredients");
+
+            driver.FindElement(By.Id("Description")).SendKeys("Test Description");
+
+            driver.FindElement(By.Id("CreateRecipe")).Click();
+
+            Assert.Equals("Create New Recipe", driver.Title);
+
+            Assert.IsTrue(true,"Create New Recipe");
+        }
+
+        [Fact]
+        public void CreateRecipeErrorMessage()
+        {
+            driver.Navigate()
+                .GoToUrl("https://localhost:44387/Home/CreateRecipe");
+
+            driver.FindElement(By.Id("RecipeName")).SendKeys("Test RecipeName");
+
+            driver.FindElement(By.Id("Ingredients")).SendKeys("Test Ingredients");
+
+            var errorMessage = driver.FindElement(By.Id("Description-error")).Text;
+
+            Assert.Equals("Description Error", errorMessage);
+        }
+    }
+
+}
--- RecipeAppTest/UnitTest1.cs
+++ RecipeAppTest/UnitTest1.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeApp.Controllers;
+using RecipeAppAPI.Controllers;
+using RecipeAppAPI.Database;
+using Xunit;
 
 namespace RecipeAppTest
 {
@@ -10,13 +16,43 @@ namespace RecipeAppTest
         [TestMethod]
         public void TestMethod1()
         {
+            //Run Reciepe App Test
+            RecipeAppTest();
 
-            RecipeApp.Controllers.HomeController homeController = new RecipeApp.Controllers.HomeController();
+            //Run Recipe App API  Test
+            RecipeAppAPITest();
+
+            //Run Recipe UI Test
+            RecipeUITest recipeUITest = new RecipeUITest();
+            recipeUITest.ReturnIndexRecipeView();
+            recipeUITest.CrateNewRecipe();
+            recipeUITest.CreateRecipeErrorMessage();
+        }
+        [Fact]
+        public void RecipeAppTest()
+        {
+            HomeController homeController = new HomeController();
             homeController.Index();
 
             homeController.UpdateRecipe(10);
 
             homeController.DeleteRecipe(10);
+
+            Assert.IsTrue(true);
+        }
+        [Fact]
+        public void RecipeAppAPITest()
+        {
+            
+            RecipeDataContext DbContext = new RecipeDataContext(null);
+            RecipeController recipeController = new RecipeController(DbContext);
+            recipeController.GetAllRecipe();
+
+            recipeController.GetRecipe(10);
+
+            recipeController.DeleteRecipe(10);
+
+            Assert.IsTrue(true);
         }
     }
 }
